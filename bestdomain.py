290cf4d7b59b829233f7d69a64d6466b2f6f035a
linlinle559 <linlinle559@gmail.com>
2025-02-27 17:08:16 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     
     # 示例URL和子域名对应的IP列表
     subdomain_ip_mapping = {
-        'yxip': 'https://ipdb.030101.xyz/api/bestcf.txt',
+        'yxip': 'https://ip.164746.xyz/ipTop10.html',
         'pyip': 'https://ipdb.030101.xyz/api/bestproxy.txt',
         # 添加更多子域名和对应的IP列表URL
     }
